8914cb84f99db21402f2e157c7a567fd15103f62
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-07-28 16:29:21 +0800

    list the files
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: directory should be provided by users
+    let files = fs::read_dir(".").unwrap();
+    files
+        .filter_map(Result::ok)
+        .filter_map(|f| {
+            if f.path().ends_with(".agda.tree") {
+                Some(f.path())
+            } else {
+                None
+            }
+        })
+        .for_each(|path| {
+            // TODO: now we get some files to deal, we need to open them
+            println!("{:?}", path);
+        });
 }
